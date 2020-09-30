5202078c210e4be96ba22c51bef045285d430355
Mads Bøgeskov <mads@bogeskov.net>
2020-09-30 12:22:19 +0200

    Added product

--- Package.swift
+++ Package.swift
@@ -6,13 +6,12 @@ import PackageDescription
 let package = Package(
     name: "SwaggerSwift",
     platforms: [.macOS(.v10_15)],
+    products: [.executable(name: "SwaggerSwift", targets: ["SwaggerSwift"])],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
         .package(url: "https://github.com/MadsBogeskov/SwaggerSwiftML", .branch("main")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwaggerSwift", dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
