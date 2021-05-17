ecd130d486fb2599506cd65d4400413393e092c2
Mads Bøgeskov <mbo@lunar.app>
2021-05-17 10:54:04 +0200

    QueryItem should support Int

--- Sources/SwaggerSwift/start.swift
+++ Sources/SwaggerSwift/start.swift
@@ -25,6 +25,14 @@ extension URLQueryItem {
     init(name: String, value: Bool) {
         self.init(name: name, value: value ? "true" : "false")
     }
+
+    init(name: String, value: Int) {
+        self.init(name: name, value: String(value))
+    }
+
+    init(name: String, value: Int64) {
+        self.init(name: name, value: String(value))
+    }
 }
 """
 
