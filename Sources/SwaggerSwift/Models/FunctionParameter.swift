ac3191ad34c99a8c8f188cfbe5e74e916d4b6c27
Mads Bøgeskov <mads@bogeskov.net>
2021-08-18 10:34:42 +0200

    Updated support for multipart uploads

--- Sources/SwaggerSwift/Models/FunctionParameter.swift
+++ Sources/SwaggerSwift/Models/FunctionParameter.swift
@@ -1,7 +1,18 @@
 /// describes a single parameter to a function
 struct FunctionParameter {
+    enum In {
+        case body
+        case formData
+        case headers
+        case path
+        case query
+        case nowhere
+    }
+
     let description: String?
     let name: String
     let typeName: TypeType
     let required: Bool
+    let `in`: In
+    let isEnum: Bool
 }
