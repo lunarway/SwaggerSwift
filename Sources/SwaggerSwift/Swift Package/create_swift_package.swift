8704fe840c9a4bb24d1a85999a6649d43e1ba6a0
Mads Bøgeskov <mbo@lunar.app>
2021-10-27 10:14:03 +0200

    Support static and dynamic frameworks

--- Sources/SwaggerSwift/Swift Package/create_swift_package.swift
+++ Sources/SwaggerSwift/Swift Package/create_swift_package.swift
@@ -15,27 +15,32 @@ func createSwiftProject(at path: String, named name: String, fileManager: FileMa
 
     // create package file
     let packageFile = """
-    // swift-tools-version:5.3
-    // The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
-    import PackageDescription
+import PackageDescription
 
-    let package = Package(
-        name: "PROJECT_NAME",
-        platforms: [.iOS(.v11)],
-        products: [.library(name: "PROJECT_NAME", targets: ["PROJECT_NAME"])],
-        dependencies: [],
-        targets: [
-            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-            // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-            .target(
-                name: "PROJECT_NAME",
-                dependencies: []),
-            .testTarget(
-                name: "PROJECT_NAMETests",
-                dependencies: ["PROJECT_NAME"]),
-        ]
-    )
+let package = Package(
+    name: "PROJECT_NAME",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(name: "PROJECT_NAME",
+                 targets: ["PROJECT_NAME"]),
+        .library(name: "PROJECT_NAMEStatic",
+                 type: .static,
+                 targets: ["PROJECT_NAME"]),
+        .library(name: "PROJECT_NAMEDynamic",
+                 type: .dynamic,
+                 targets: ["PROJECT_NAME"])
+    ],
+    targets: [
+        .target(name: "PROJECT_NAME"),
+        .testTarget(
+            name: "PROJECT_NAMETests",
+            dependencies: ["PROJECT_NAME"]),
+    ]
+)
+
     """
 
     try packageFile
