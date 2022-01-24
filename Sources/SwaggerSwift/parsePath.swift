b6c55b08f8d34d5882a8e0dfebf482f14a475757
Maximilian Scheid <max@lunar.app>
2022-01-24 13:12:27 +0100

    Slightly better solution

--- Sources/SwaggerSwift/parsePath.swift
+++ Sources/SwaggerSwift/parsePath.swift
@@ -7,58 +7,44 @@ func parse(path: SwaggerSwiftML.Path, servicePath: String, swagger: Swagger, swa
 
     var functions = [NetworkRequestFunction]()
     var modelDefinitions = [ModelDefinition]()
-    if let operation = path.get {
-        guard let result = parse(operation: operation, httpMethod: .get, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose) else {
-            return (functions, modelDefinitions)
-        }
+    if let operation = path.get,
+       let result = parse(operation: operation, httpMethod: .get, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose) {
         functions.append(result.0)
         modelDefinitions.append(contentsOf: result.1)
     }
 
-    if let operation = path.put {
-        guard let result = parse(operation: operation, httpMethod: .put, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose) else {
-            return (functions, modelDefinitions)
-        }
+    if let operation = path.put,
+       let result = parse(operation: operation, httpMethod: .put, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose) {
         functions.append(result.0)
         modelDefinitions.append(contentsOf: result.1)
     }
 
-    if let operation = path.post {
-        guard let result = parse(operation: operation, httpMethod: .post, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose)  else {
-            return (functions, modelDefinitions)
-        }
+    if let operation = path.post,
+       let result = parse(operation: operation, httpMethod: .post, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose){
         functions.append(result.0)
         modelDefinitions.append(contentsOf: result.1)
     }
 
-    if let operation = path.delete {
-        guard let result = parse(operation: operation, httpMethod: .delete, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose) else {
-            return (functions, modelDefinitions)
-        }
+    if let operation = path.delete,
+       let result = parse(operation: operation, httpMethod: .delete, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose) {
         functions.append(result.0)
         modelDefinitions.append(contentsOf: result.1)
     }
 
-    if let operation = path.options {
-        guard let result = parse(operation: operation, httpMethod: .options, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose)  else {
-            return (functions, modelDefinitions)
-        }
+    if let operation = path.options,
+       let result = parse(operation: operation, httpMethod: .options, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose) {
         functions.append(result.0)
         modelDefinitions.append(contentsOf: result.1)
     }
 
-    if let operation = path.head {
-        guard let result = parse(operation: operation, httpMethod: .head, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose)  else {
-            return (functions, modelDefinitions)
-        }
+    if let operation = path.head,
+       let result = parse(operation: operation, httpMethod: .head, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose){
         functions.append(result.0)
         modelDefinitions.append(contentsOf: result.1)
     }
 
-    if let operation = path.patch {
-        guard let result = parse(operation: operation, httpMethod: .patch, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose) else {
-            return (functions, modelDefinitions)
-        }
+    if let operation = path.patch,
+       let result = parse(operation: operation, httpMethod: .patch, servicePath: servicePath, parameters: parameters, swagger: swagger, swaggerFile: swaggerFile, verbose: verbose){
         functions.append(result.0)
         modelDefinitions.append(contentsOf: result.1)
     }
