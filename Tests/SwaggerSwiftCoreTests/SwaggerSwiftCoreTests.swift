d85fe5de4331b8dc9b389adcda45be141a05397d
Magnus Kahr Jensen <maj@lunar.app>
2022-03-14 09:41:25 +0100

    URL decoding now goes through string initializer

--- Tests/SwaggerSwiftCoreTests/SwaggerSwiftCoreTests.swift
+++ Tests/SwaggerSwiftCoreTests/SwaggerSwiftCoreTests.swift
@@ -1,14 +1,45 @@
 import XCTest
+@testable import SwaggerSwiftCore
 
-final class SwaggerSwiftCoreTests: XCTestCase {
-    func testExample() {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct
-//        // results.
-//        XCTAssertEqual(SwaggerSwift().text, "Hello, World!")
+final class SwaggerSwiftTests: XCTestCase {
+    
+    let swaggerFile = SwaggerFile(path: "", organisation: "", services: [:], globalHeaders: nil)
+    
+    func testURLDecodeFormat() {
+        
+        let model = Model(description: nil,
+                          typeName: "Test",
+                          fields: [.init(description: nil, type: .object(typeName: "URL"), name: "url", required: false)],
+                          inheritsFrom: [],
+                          isInternalOnly: false,
+                          embeddedDefinitions: [],
+                          isCodable: true)
+
+        let result = model.modelDefinition(serviceName: nil,
+                                           swaggerFile: swaggerFile)
+
+        XCTAssertEqual(result, """
+public struct Test: Codable {
+    public let url: URL?
+
+    public init(url: URL?) {
+        self.url = url
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        // Allows the backend to return badly formatted urls
+        if let urlString = try container.decodeIfPresent(String.self, forKey: .url) {
+            self.url = URL(string: urlString)
+        } else {
+            self.url = nil
+        }
+    }
+}
+""")
     }
 
     static var allTests = [
-        ("testExample", testExample)
+        ("testExample", testURLDecodeFormat)
     ]
 }
