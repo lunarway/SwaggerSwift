1ca00ed449452eabb5228c32dec8b5ec0955e4bc
Mads Bøgeskov <mbo@lunar.app>
2022-03-15 09:22:49 +0100

    das

--- Sources/SwaggerSwiftCore/Swift Package/create_swift_package.swift
+++ Sources/SwaggerSwiftCore/Swift Package/create_swift_package.swift
@@ -58,7 +58,7 @@ class SwiftPackageBuilder {
 
         let packageFile =
     """
-    // swift-tools-version:5.3
+    // swift-tools-version:5.5
     // The swift-tools-version declares the minimum version of Swift required to build this package.
     
     import PackageDescription
