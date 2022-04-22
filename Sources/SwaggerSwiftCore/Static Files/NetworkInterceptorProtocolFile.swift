906468d8950e7840b6b50c61611de4e418df944e
Mads Bøgeskov <mbo@lunar.app>
2022-04-22 12:23:03 +0200

    Added callback if network fails to parse object (#88)

--- Sources/SwaggerSwiftCore/Static Files/NetworkInterceptorProtocolFile.swift
+++ Sources/SwaggerSwiftCore/Static Files/NetworkInterceptorProtocolFile.swift
@@ -4,5 +4,13 @@ import Foundation
 public protocol NetworkInterceptor {
     func networkWillPerformRequest(_ request: URLRequest) -> URLRequest
     func networkDidPerformRequest(urlRequest: URLRequest, urlResponse: URLResponse?, data: Data?, error: Error?) -> Bool
+    /// Called when the network fails to parse a response object as based on the Swagger spec. This probably means that the Swagger spec is in-compliant with the actual API.
+    /// - Parameters:
+    ///   - urlRequest: the URLRequest
+    ///   - urlResponse: the URLResponse
+    ///   - data: the payload
+    ///   - error: the parsing error
+    func networkFailedToParseObject(urlRequest: URLRequest, urlResponse: URLResponse?, data: Data?, error: Error?)
 }
+
 """
