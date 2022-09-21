cc6a3aca1cd6d20bdea4d55595b2e09cc8d41cf4
Maximilian Scheid <max@lunar.app>
2022-09-21 10:58:09 +0200

    Adjust Interceptor protocol (#113)
    * Adjust Interceptor protocol
    
    * Non optional completion, and if statement
    
    * escaping
    
    * else

--- Sources/SwaggerSwiftCore/Static Files/NetworkInterceptorProtocolFile.swift
+++ Sources/SwaggerSwiftCore/Static Files/NetworkInterceptorProtocolFile.swift
@@ -3,7 +3,7 @@ import Foundation
 
 <ACCESSCONTROL> protocol NetworkInterceptor {
     func networkWillPerformRequest(_ request: URLRequest) -> URLRequest
-    func networkDidPerformRequest(urlRequest: URLRequest, urlResponse: URLResponse?, data: Data?, error: Error?) -> Bool
+    func networkDidPerformRequest(urlRequest: URLRequest, urlResponse: URLResponse?, data: Data?, error: Error?, completion: @escaping ((Result<Void, Error>) -> Void))
     /// Called when the network fails to parse a response object as based on the Swagger spec. This probably means that the Swagger spec is in-compliant with the actual API.
     /// - Parameters:
     ///   - urlRequest: the URLRequest
