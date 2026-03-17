ae8d5aacfaa421af043138734032095a3eebf126
MadsBogeskov <mbo@lunar.app>
2026-03-17 12:41:17 +0100

    Revert ":recycle: move parse request logic into APIRequestFactory"
    This reverts commit b643f17efef7673a6f650272d19cd63c17ef5f67.

--- Sources/SwaggerSwiftCore/API Request Factory/APIRequestFactory.swift
+++ Sources/SwaggerSwiftCore/API Request Factory/APIRequestFactory.swift
@@ -51,7 +51,7 @@ public struct APIRequestFactory {
 
             guard let requestResponse = $0.value else { return nil }
 
-            if let (responseType, embeddedDefinitions) = try parseResponse(
+            if let (responseType, embeddedDefinitions) = try parse(
                 request: requestResponse,
                 httpMethod: httpMethod,
                 servicePath: servicePath,
@@ -143,91 +143,6 @@ public struct APIRequestFactory {
         )
 
         return (apiRequest, inlineResponseModels)
-    }
-
-    private func parseResponse(
-        request requestNode: Node<SwaggerSwiftML.Response>,
-        httpMethod: HTTPMethod,
-        servicePath: String,
-        statusCode: Int,
-        swagger: Swagger,
-        modelTypeResolver: ModelTypeResolver
-    ) throws -> (TypeType, [ModelDefinition])? {
-        let requestName =
-            servicePath
-            .replacingOccurrences(of: "{", with: "")
-            .replacingOccurrences(of: "}", with: "")
-            .components(separatedBy: "/")
-            .map { $0.components(separatedBy: "-") }
-            .flatMap { $0 }
-            .map { $0.uppercasingFirst }
-            .joined()
-            .capitalizingFirstLetter()
-
-        let prefix = "\(requestName)\(statusCode)"
-
-        let request: SwaggerSwiftML.Response
-        switch requestNode {
-        case .reference(let reference):
-            let modelReference = try ModelReference(rawValue: reference)
-
-            switch modelReference {
-            case .definitions:
-                guard
-                    let schema = try? swagger.findSchema(reference: modelReference.typeName)
-                else {
-                    log(
-                        "[\(swagger.serviceName) \(httpMethod) \(servicePath)] : Failed to find referenced definitions object: \(reference)",
-                        error: true
-                    )
-                    return nil
-                }
-
-                request = SwaggerSwiftML.Response(schema: schema)
-            case .responses:
-                guard
-                    let responseObject = swagger.responses?.first(where: {
-                        $0.key.lowercased() == modelReference.typeName.lowercased()
-                    })?.value
-                else {
-                    log(
-                        "[\(swagger.serviceName) \(httpMethod) \(servicePath)]: Failed to find referenced response object: \(reference)",
-                        error: true
-                    )
-                    return nil
-                }
-
-                request = responseObject
-            }
-        case .node(let node):
-            request = node
-        }
-
-        if let schemaNode = request.schema {
-            switch schemaNode {
-            case .node(let schema):
-                let resolvedType = try modelTypeResolver.resolve(
-                    forSchema: schema,
-                    typeNamePrefix: prefix,
-                    namespace: swagger.serviceName,
-                    swagger: swagger
-                )
-                return (resolvedType.propertyType, resolvedType.inlineModelDefinitions)
-            case .reference(let ref):
-                let schema = try swagger.findSchema(reference: ref)
-                let modelReference = try ModelReference(rawValue: ref)
-
-                let resolvedType = schema.type(named: modelReference.typeName)
-
-                if case .array = resolvedType {
-                    return (TypeType.object(typeName: modelReference.typeName), [])
-                } else {
-                    return (resolvedType, [])
-                }
-            }
-        } else {
-            return (.void, [])
-        }
     }
 
     /// Create the list of URLQueryItem assignments that needs to be performed in the body of the request. This is not to
